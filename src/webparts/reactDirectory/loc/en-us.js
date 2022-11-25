d4cc0716f71d71ddce0e6c19479f77944860902a
MoonSky1! <vanmathyrr@gmail.com>
2022-11-25 14:33:12 -0500

    space deleted

--- src/webparts/reactDirectory/loc/en-us.js
+++ src/webparts/reactDirectory/loc/en-us.js
@@ -5,8 +5,8 @@ define([], function () {
     BasicGroupName: "Properties",
     TitleFieldLabel: "Web Part Title",  
     PagingLabel: "Results per page",
-    DirectoryMessage: "Description: Hmmm… We couldn’t find the user you were looking for. They might not yet have joined GCXchange. Why not invite them, so you can work better, together.​",
+    DirectoryMessage: "Description: Hmmm… We couldn’t find the user you were looking for. They might not yet have joined GCXchange. Why not invite them, so you can work better, together.",
     LoadingText: "Searching for user. Please wait...",
-    SearchBoxLabel:"Search for a colleague (First Name)​",
+    SearchBoxLabel:"Search for a colleague (First Name)",
   };
 });
